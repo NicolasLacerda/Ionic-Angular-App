0a476580b3c73e79a0139e7fc30b56c556d27e8f
NicolasLacerda <nicolas.lacerda.work@gmail.com>
2024-09-06 15:51:41 -0300

    06/09/2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NativePageTransitions } from '@awesome-cordova-plugins/native-page-transitions';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  NativePageTransitions,
+  NativeTransitionOptions,
+} from '@awesome-cordova-plugins/native-page-transitions/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
-
 }
