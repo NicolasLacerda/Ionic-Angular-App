36c68f9c516623e4633a5b6595bde2b800f22bf3
NicolasLacerda <nicolas.lacerda.work@gmail.com>
2024-09-20 17:07:31 -0300

    20/09/2024
    Adicionada função de pintar diferentes partes do carro.

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -28,6 +28,7 @@ export class Tab5Page implements OnInit {
           '.png" style="width:75%; padding:10px" >'
       );
     }
+    $('.other-colors').css('display', 'none');
 
     setTimeout(() => {
       let vinils: any = localStorage.getItem('colorGroup');
@@ -36,19 +37,38 @@ export class Tab5Page implements OnInit {
       for (var i = 0; i < vinils.length; i++) {
         if (i <= 4) {
           $('.colors').append(
-            '<button class="colors-item" ng-click="{()=>{console.log("dadada")}}" value=' +
+            '<button class="colors-item" value=' +
               vinil[i] +
               ' style="width:2.3rem; height:2.3rem; border-radius: 10px; background: #' +
               vinil[i] +
               '" ></button>'
           );
+        } else if (i > 4 && i < 25) {
+          $('.colors2').append(
+            '<button class="colors-item" value=' +
+              vinil[i] +
+              ' style="width:3rem; height:3rem; border-radius: 10px; background: #' +
+              vinil[i] +
+              '" ></button>'
+          );
         }
       }
+
+      $('#upBtn').on('click', () => {
+        $('.arrowUp').toggleClass('rotate');
+        $('.other-colors').slideToggle('slow');
+      });
+
+      $('#wrapper').on('click', function () {
+        $(this).toggleClass('open');
+        $('.btnR').slideToggle('slow');
+      });
     }, 100);
 
+    //Canvas
     setTimeout(() => {
       //Render
-      let renderer = new THREE.WebGLRenderer({ antialias: true });
+      let renderer = new THREE.WebGLRenderer({});
       renderer.setSize(window.innerWidth, window.innerHeight);
 
       renderer.setClearColor(0x000000);
@@ -62,7 +82,7 @@ export class Tab5Page implements OnInit {
         50,
         window.innerWidth / window.innerHeight,
         0.1,
-        150
+        15
       );
       //x = width / left and right
       //y = height / top and bottom
@@ -73,20 +93,20 @@ export class Tab5Page implements OnInit {
       //Orbit Control
       let controls = new OrbitControls(camera, renderer.domElement);
       document.body.appendChild(renderer.domElement);
-      controls.target.set(0, 0, 0);
+      controls.target.set(0, 0.3, 0);
       controls.maxPolarAngle = 1.4;
       controls.enableDamping = false; //Coloca peso na camera
       controls.enablePan = false; //Permite arrastar o objeto
       controls.autoRotate = false; //Roda o objeto.
-      controls.minDistance = 3;
-      controls.maxDistance = 15;
+      controls.minDistance = 4;
+      controls.maxDistance = 12;
       controls.update();
 
       //Scene
       let scene: any = new THREE.Scene();
 
       //Light
-      let light = new THREE.SpotLight(0xffffff, 350);
+      let light = new THREE.SpotLight(0xffffff, 250);
       light.position.set(0, 10, 0);
       light.distance = 20;
       light.angle = 0.4;
@@ -94,19 +114,19 @@ export class Tab5Page implements OnInit {
       light.castShadow = true;
       scene.add(light);
 
-      let dirlight = new THREE.DirectionalLight(0xffffff, 4);
+      let dirlight = new THREE.DirectionalLight(0xffffff, 2);
       dirlight.position.set(5, 0, 0);
       scene.add(dirlight);
 
-      let dirlight2 = new THREE.DirectionalLight(0xffffff, 4);
+      let dirlight2 = new THREE.DirectionalLight(0xffffff, 2);
       dirlight2.position.set(-5, 0, 0);
       scene.add(dirlight2);
 
-      let dirlight3 = new THREE.DirectionalLight(0xffffff, 4);
+      let dirlight3 = new THREE.DirectionalLight(0xffffff, 2);
       dirlight3.position.set(0, 0, 5);
       scene.add(dirlight3);
 
-      let dirlight4 = new THREE.DirectionalLight(0xffffff, 4);
+      let dirlight4 = new THREE.DirectionalLight(0xffffff, 2);
       dirlight4.position.set(0, 0, -5);
       scene.add(dirlight4);
 
@@ -129,7 +149,9 @@ export class Tab5Page implements OnInit {
 
       //GET LOCALSTORAGE
       let selectedCarUrl: any = localStorage.getItem('carUrl');
-      let carUrl = '../../assets/scenes/' + selectedCarUrl + '.glb';
+      let carUrl = '../../assets/scenes/honda/fit/fit-2007.glb';
+
+      const lod = new THREE.LOD();
 
       //Model
       let loader = new GLTFLoader();
@@ -138,7 +160,15 @@ export class Tab5Page implements OnInit {
         mesh.castShadow = true;
         mesh.traverse((child) => {
           child.castShadow = true;
+          let vinils: any = localStorage.getItem('colorGroup');
+          let vinil: any = vinils.split(',');
           let body = child.getObjectByName('body');
+          let roof = child.getObjectByName('teto');
+          let retro = child.getObjectByName('retro');
+          $('.retro').removeClass('active-btn');
+          $('.teto').removeClass('active-btn');
+          $('.todos').addClass('active-btn');
+
           body?.traverse((wrap) => {
             if (wrap instanceof THREE.Mesh) {
               wrap.material.color.setHex(0xffffff);
@@ -150,9 +180,182 @@ export class Tab5Page implements OnInit {
               });
             }
           });
-          if (child instanceof THREE.Mesh) {
-          }
+
+          roof?.traverse((wrap) => {
+            if (wrap instanceof THREE.Mesh) {
+              wrap.material.color.setHex(0xffffff);
+              let colors = $('.colors-item');
+              $('.colors-item').on('click', function (e) {
+                $(colors).css('border', '0px');
+                $(this).css('border', '3px solid #49ade7');
+                wrap.material.color.setHex('0x' + $(this).attr('value'));
+              });
+            }
+          });
+
+          retro?.traverse((wrap) => {
+            if (wrap instanceof THREE.Mesh) {
+              wrap.material.color.setHex(0xffffff);
+              let colors = $('.colors-item');
+              $('.colors-item').on('click', function (e) {
+                $(colors).css('border', '0px');
+                $(this).css('border', '3px solid #49ade7');
+                wrap.material.color.setHex('0x' + $(this).attr('value'));
+              });
+            }
+          });
+
+          //Botão Teto
+          $('.teto').on('click', function (e) {
+            $('.todos').removeClass('active-btn');
+            $('.retro').removeClass('active-btn');
+            $('.teto').addClass('active-btn');
+
+            $('.colors-item-roof').remove();
+            for (var i = 0; i < vinils.length; i++) {
+              if (i <= 4) {
+                $('.colors').append(
+                  '<button class="colors-item-roof" value=' +
+                    vinil[i] +
+                    ' style="width:2.3rem; height:2.3rem; border-radius: 10px; background: #' +
+                    vinil[i] +
+                    '" ></button>'
+                );
+              } else if (i > 4 && i < 25) {
+                $('.colors2').append(
+                  '<button class="colors-item-roof" value=' +
+                    vinil[i] +
+                    ' style="width:3rem; height:3rem; border-radius: 10px; background: #' +
+                    vinil[i] +
+                    '" ></button>'
+                );
+              }
+            }
+            $('.colors-item').remove();
+            $('.colors-item-retro').remove();
+
+            setTimeout(() => {
+              roof?.traverse((wrap) => {
+                if (wrap instanceof THREE.Mesh) {
+                  let colorsRoof = $('.colors-item-roof');
+                  $('.colors-item-roof').on('click', function (e) {
+                    $(colorsRoof).css('border', '0px');
+                    $(this).css('border', '3px solid #49ade7');
+                    wrap.material.color.setHex('0x' + $(this).attr('value'));
+                  });
+                }
+              });
+            }, 100);
+          });
+
+          //Botão Retrovisor
+          $('.retro').on('click', function (e) {
+            $('.todos').removeClass('active-btn');
+            $('.teto').removeClass('active-btn');
+            $('.retro').addClass('active-btn');
+
+            $('.colors-item-retro').remove();
+            for (var i = 0; i < vinils.length; i++) {
+              if (i <= 4) {
+                $('.colors').append(
+                  '<button class="colors-item-retro" value=' +
+                    vinil[i] +
+                    ' style="width:2.3rem; height:2.3rem; border-radius: 10px; background: #' +
+                    vinil[i] +
+                    '" ></button>'
+                );
+              } else if (i > 4 && i < 25) {
+                $('.colors2').append(
+                  '<button class="colors-item-retro" value=' +
+                    vinil[i] +
+                    ' style="width:3rem; height:3rem; border-radius: 10px; background: #' +
+                    vinil[i] +
+                    '" ></button>'
+                );
+              }
+            }
+            $('.colors-item').remove();
+            $('.colors-item-roof').remove();
+
+            setTimeout(() => {
+              retro?.traverse((wrap) => {
+                if (wrap instanceof THREE.Mesh) {
+                  let colorsRetro = $('.colors-item-retro');
+                  $('.colors-item-retro').on('click', function (e) {
+                    $(colorsRetro).css('border', '0px');
+                    $(this).css('border', '3px solid #49ade7');
+                    wrap.material.color.setHex('0x' + $(this).attr('value'));
+                  });
+                }
+              });
+            }, 125);
+          });
+
+          //Botão Carro Inteiro
+          $('.todos').on('click', function (e) {
+            $('.retro').removeClass('active-btn');
+            $('.teto').removeClass('active-btn');
+            $('.todos').addClass('active-btn');
+
+            $('.colors-item').remove();
+            for (var i = 0; i < vinils.length; i++) {
+              if (i <= 4) {
+                $('.colors').append(
+                  '<button class="colors-item" value=' +
+                    vinil[i] +
+                    ' style="width:2.3rem; height:2.3rem; border-radius: 10px; background: #' +
+                    vinil[i] +
+                    '" ></button>'
+                );
+              } else if (i > 4 && i < 25) {
+                $('.colors2').append(
+                  '<button class="colors-item" value=' +
+                    vinil[i] +
+                    ' style="width:3rem; height:3rem; border-radius: 10px; background: #' +
+                    vinil[i] +
+                    '" ></button>'
+                );
+              }
+            }
+            $('.colors-item-retro').remove();
+            $('.colors-item-roof').remove();
+
+            setTimeout(() => {
+              body?.traverse((wrap) => {
+                if (wrap instanceof THREE.Mesh) {
+                  let colors = $('.colors-item');
+                  $('.colors-item').on('click', function (e) {
+                    $(colors).css('border', '0px');
+                    $(this).css('border', '3px solid #49ade7');
+                    wrap.material.color.setHex('0x' + $(this).attr('value'));
+                  });
+                }
+              });
+              roof?.traverse((wrap) => {
+                if (wrap instanceof THREE.Mesh) {
+                  let colors = $('.colors-item');
+                  $('.colors-item').on('click', function (e) {
+                    $(colors).css('border', '0px');
+                    $(this).css('border', '3px solid #49ade7');
+                    wrap.material.color.setHex('0x' + $(this).attr('value'));
+                  });
+                }
+              });
+
+              retro?.traverse((wrap) => {
+                if (wrap instanceof THREE.Mesh) {
+                  let colors = $('.colors-item');
+                  $('.colors-item').on('click', function (e) {
+                    $(colors).css('border', '0px');
+                    $(this).css('border', '3px solid #49ade7');
+                    wrap.material.color.setHex('0x' + $(this).attr('value'));
+                  });
+                }
+              });
+            }, 125);
+          });
         });
+        console.log(mesh);
         scene.add(mesh);
         animate();
       });
